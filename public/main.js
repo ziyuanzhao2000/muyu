25ccf9271d97cee3938dbc03061db54db2a6c3a3
Ziyuan Zhao <ziyuanzhao@college.harvard.edu>
2023-10-06 19:59:33 -0400

    Add skybox and enable antialias

--- public/main.js
+++ public/main.js
@@ -10,12 +10,48 @@ const audio = new Audio('./wood_hit.wav');
 
 // setup the scene
 const scene = new THREE.Scene();
-scene.fog = new THREE.Fog( 0xcccccc, 0, 10);
+// scene.fog = new THREE.Fog( 0xcccccc, 0, 10);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// add skybox
+// scene.background = new THREE.CubeTextureLoader()
+//     .setPath('skybox/')
+//     .load([
+//         'px.png',
+//         'nx.png',
+//         'py.png',
+//         'ny.png',
+//         'pz.png',
+//         'nz.png'
+// ]);
+const skyboxTexture = new THREE.CubeTextureLoader()
+    .setPath('skybox/')
+    .load([
+        'px.png',
+        'nx.png',
+        'py.png',
+        'ny.png',
+        'pz.png',
+        'nz.png'
+]);
+const paths = ['skybox/py.png',
+            'skybox/ny.png',
+            'skybox/pz.png',
+            'skybox/nz.png',
+            'skybox/px.png',
+            'skybox/nx.png'];
+
+const skyboxMaterial = paths.map(p => new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load(p), 
+        side: THREE.DoubleSide
+    })
+);
+const skybox = new THREE.Mesh(new THREE.BoxGeometry(100,100,100), skyboxMaterial);
+scene.add(skybox);
 
 
 // add rendering engine and orbit control
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -23,15 +59,20 @@ document.body.appendChild( renderer.domElement );
 let loader = new GLTFLoader();
 var obj;
 var controls;
+const scale_factor = 20;
 loader.load( 'model_1.gltf', function ( gltf ) {
 	scene.add( gltf.scene );
     obj = gltf.scene;
-    obj.scale.x = 10
-    obj.scale.y = 10
-    obj.scale.z = 10
+    obj.scale.x = scale_factor;
+    obj.scale.y = scale_factor;
+    obj.scale.z = scale_factor;
     let mesh_list = obj.children[0].children;
     mesh_list.forEach(mesh => mesh.name='object');
+    mesh_list.push(skybox);
     controls = new ObjectControls(camera, renderer.domElement, mesh_list)
+    controls.setRotationSpeed(0.1);
+    controls.setMaxVerticalRotationAngle(Math.PI / 4, Math.PI / 4);
+    controls.setMaxHorizontalRotationAngle(Math.PI, Math.PI);
     controls.enableVerticalRotation(); // enables the vertical rotation
     controls.disableZoom();
 }, undefined, function ( error ) {
@@ -45,12 +86,19 @@ const stick = new THREE.Mesh (stick_geometry,
         fog: true
     } )); 
 scene.add( stick );
-stick.position.set(0, 0, 1);
+const stickDistance = 2;
+stick.position.set(0, 0, stickDistance);
 
 // add lighting and misc elmts
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
-ambientLight.intensity = 4;
+ambientLight.intensity = 1;
 scene.add( ambientLight );
+const pointLight = new THREE.PointLight( 0xFFFFFF, 1, 100 );
+pointLight.position.set( 0, 0, 0 );
+scene.add( pointLight );
+const dirLight1 = new THREE.DirectionalLight( 0xffddcc, 5);
+dirLight1.position.set( 1, 0.75, 0.5 );
+scene.add( dirLight1 );
 
 const axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
@@ -64,7 +112,7 @@ const pointer = new THREE.Vector2();
 function projectMousePosition(x, y) {
     pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-    const Zplane = new THREE.Vector3(0,0,1).project(camera).z;
+    const Zplane = new THREE.Vector3(0,0,stickDistance).project(camera).z;
     return new THREE.Vector3(pointer.x, 
                             pointer.y, 
                             Zplane).unproject( camera );
@@ -103,7 +151,7 @@ function hitObject() {
 }
 
 function animate() {
-
+    // skybox.rotation.x += 0.001;
     raycaster.setFromCamera( pointer, camera );
 	const intersects = raycaster.intersectObjects( scene.children );
     const currPointerOnObj = intersects.some(x => x.object.name=="object")
